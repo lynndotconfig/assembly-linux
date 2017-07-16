6fb4727c8e195403d5db3c79f0e6a13b22de90e4
lynndotconfig <lynn.config@gmail.com>
2017-07-16 23:36:24 +0800

    add basic

---
+++ csapp/basic/multi2.c
@@ -0,0 +1,6 @@
+long multi2(long, long);
+
+void multistore(long x, long y, long *dest) {
+	long t = multi2(x, y);
+	*dest = t;
+}
---
+++ csapp/basic/multi2.s
@@ -0,0 +1,21 @@
+	.file	"multi2.c"
+	.text
+	.globl	multistore
+	.type	multistore, @function
+multistore:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movq	%rdx, %rbx
+	call	multi2
+	movq	%rax, (%rbx)
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	multistore, .-multistore
+	.ident	"GCC: (Ubuntu 5.2.1-22ubuntu2) 5.2.1 20151010"
+	.section	.note.GNU-stack,"",@progbits
